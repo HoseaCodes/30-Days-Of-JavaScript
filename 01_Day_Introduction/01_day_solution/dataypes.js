c0499a6cd54b5f8e59062edd9ede8f842c02f13c
hosead6168 <dominique11h@yahoo.com>
2020-10-04 17:21:48 -0500

    Day 1 completed

---
+++ 01_Day_Introduction/01_day_solution/dataypes.js
@@ -0,0 +1,50 @@
+// String Data Type
+const name = 'Dominique'
+
+// Boolean Data Type
+const isOn = True;
+
+// Undefined Data Type
+const home
+
+// Null Data Type
+const none = null;
+
+// Typeof operator
+console.log(typeof name)
+console.log(typeof isOn)
+console.log(typeof home)
+console.log(typeof none)
+
+// Declare four variable without assigning values
+const one
+const two
+const three
+const four
+
+// Declare four variables with assigned values
+const toy = bear;
+const car = BMW;
+const tv = Samsung;
+const shirt = BR;
+
+// Declare variables to store your first name, last name, martial status, country and age in multiple lines
+const firstName = 'Dominique';
+const lastName = 'Hosea';
+const isMarried = False;
+const country = 'US';
+const age = 28;
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let fName = 'Dominique',
+    lName = 'Hosea',
+    isSingle = True,
+    country = 'US',
+    age = 28;
+
+// Delcare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+const myAge = 28;
+const yourAge = 30;
+
+console.log(`My age ` + myAge, `Your age ` + yourAge)
---
+++ 01_Day_Introduction/01_day_solution/variable.js
@@ -0,0 +1,16 @@
+// Comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/* Comments can make code readable,
+easy to reuse and informative */
+
+// String Data Type
+const name = 'Dominique'
+
+// Boolean Data Type
+const isOn = True;
+
+// Undefined Data Type
+const home
+
+// Null Data Type
+const none = null;
